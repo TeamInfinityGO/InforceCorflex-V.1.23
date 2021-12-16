cdaac058e4db828061058773717235e3f77a8861
TeamInfinityGO <95580943+TeamInfinityGO@users.noreply.github.com>
2021-12-16 10:23:32 +0530

    Setup And Main

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='InforceCorflex',
+    version='V.1.23',
+    packages=[''],
+    url='lakgamers447@gmail.com',
+    license='Mozilla Public Licence',
+    author='DELL',
+    author_email='lakgamers447@gmail.com',
+    description='Inforce Browser'
+)
